0873bf0667bee222bf7a5d9de65f4d5863021454
Aml Fares <amlfares@paymob.com>
2021-10-31 15:17:40 +0200

    v4

--- lib/test.php
+++ lib/test.php
@@ -4,26 +4,28 @@ use Paymob\Paymob;
 
 /*test create function */
 $secret_key="skt_a67ba5b0a9cfb19e37a685f9216ebf456ddc63aa9706d21c370571269689191b";
-$app=new Paymob($secret_key);
-$body=array(    $amount=1000,
-                $currency="EGP",
-                $payment_methods=["card", "kiosk"],
-                $billing_data=[
-                    "apartment"=> "803",
-                    "email"=> "claudette09@exa.com",
-                    "floor"=> "42",
-                    "first_name"=> "Clifford",
-                    "street"=> "Ethan Land",
-                    "building"=> "8028",
-                    "phone_number"=> "+86(8)9135210487",
-                    "shipping_method"=> "PKG",
-                    "postal_code"=> "01898",
-                    "city"=> "Jaskolskiburgh",
-                    "country"=> "CR",
-                    "last_name"=> "Nicolas",
-                    "state"=> "Utah",
-                ],
-                $delivery_needed=False,);
+$base_url="http://127.0.0.1:8000/api/next";
+$api_version="v1";
+$app=new Paymob($secret_key,$base_url,$$api_version);
+$body=array($amount=1000,
+            $currency="EGP",
+            $payment_methods=["card", "kiosk"],
+            $billing_data=[
+                "apartment"=> "803",
+                "email"=> "claudette09@exa.com",
+                "floor"=> "42",
+                "first_name"=> "Clifford",
+                "street"=> "Ethan Land",
+                "building"=> "8028",
+                "phone_number"=> "+86(8)9135210487",
+                "shipping_method"=> "PKG",
+                "postal_code"=> "01898",
+                "city"=> "Jaskolskiburgh",
+                "country"=> "CR",
+                "last_name"=> "Nicolas",
+                "state"=> "Utah",
+            ],
+            $delivery_needed=False,);
 echo $app->intent->create('',$secret_key,$body);
 /*test list function */
 $secret_key="skt_a67ba5b0a9cfb19e37a685f9216ebf456ddc63aa9706d21c370571269689191b";
@@ -40,7 +42,7 @@ echo $app->intent->retrieve('',$secret_key);
 
 $secret_key="skt_a67ba5b0a9cfb19e37a685f9216ebf456ddc63aa9706d21c370571269689191b";
 $app=new Paymob($secret_key);
-$body=array(    $payment_reference="14394788",
+$body=array($payment_reference="14394788",
                 );
 echo $app->payment_reference->void('',$secret_key,$body);
 
@@ -48,9 +50,8 @@ echo $app->payment_reference->void('',$secret_key,$body);
 
 $secret_key="skt_a67ba5b0a9cfb19e37a685f9216ebf456ddc63aa9706d21c370571269689191b";
 $app=new Paymob($secret_key);
-$body=array( $payment_reference="14394788",
+$body=array($payment_reference="14394788",
              $amount="300"
-
             );
 echo $app->payment_reference->refund('',$secret_key,$body);
 
@@ -59,7 +60,7 @@ echo $app->payment_reference->refund('',$secret_key,$body);
 
 $secret_key="skt_a67ba5b0a9cfb19e37a685f9216ebf456ddc63aa9706d21c370571269689191b";
 $app=new Paymob($secret_key);
-$body=array( $payment_reference="14394788",
+$body=array($payment_reference="14394788",
              $amount="300"
 
             );
